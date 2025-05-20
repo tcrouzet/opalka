43a64a934e760931552065d5a8a3451d0395d638
tcrouzet <tc@tcrouzet.com>
2025-05-20 10:34:39 +0200

    sync 2025-05-20 10:33:39

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# opalka
+# Roman Opalka generator
+
+Pour mieux comprendre le travail monumental d'Oplaka, le reproduire a vec un bout de code.
+
+Il ainsi possible de générer des images géantes avec une multitude de détail.
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-import os, random
+import os, random, re
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 output_dir = os.path.join(script_dir, "output")
@@ -28,7 +28,24 @@ def new_painting(width, height, painting_num, number=None):
     
     draw.rectangle([(0, 0), (width, height)], fill=background)
 
-    return image, draw
+    return image, draw, background
+
+
+def opacity(digit_painted, background):
+    # Calculer l'opacité du blanc
+    opacity = 255 - digit_painted * 10
+    if opacity < 0:
+        opacity = 0
+
+    # Calculer la couleur résultante en mélangeant le blanc avec le fond
+    alpha = opacity / 255.0
+    blended_color = (
+        int(background[0] * (1 - alpha) + 255 * alpha),
+        int(background[1] * (1 - alpha) + 255 * alpha),
+        int(background[2] * (1 - alpha) + 255 * alpha)
+    )
+    return blended_color
+
 
 
 def create_opalka_painting(painting_num, number, height=9000):
@@ -59,7 +76,7 @@ def create_opalka_painting(painting_num, number, height=9000):
     # Nombre de digit avec une charge du pinceau
     treshold = random.randint(12, 22)
 
-    image, draw = new_painting(width, height, painting_num)
+    image, draw, background = new_painting(width, height, painting_num)
 
     while(True):
         # Création d'une nouvelle image
@@ -84,9 +101,7 @@ def create_opalka_painting(painting_num, number, height=9000):
                     return number + 1
 
             # Calculer l'intensité du blanc
-            brush_intensity = 255 - digit_painted*10
-            white_intensity = int(brush_intensity)
-            white = (white_intensity, white_intensity, white_intensity)
+            white = opacity(digit_painted, background)
 
             # Y aléaloite
             y_offset = random.choice([-1, 0, 0, 0, 1])
@@ -104,11 +119,82 @@ def create_opalka_painting(painting_num, number, height=9000):
         number += 1
 
 
-create_opalka_painting(30, 1)
-exit()
+def extract_number(filename):
+    # Utiliser une expression régulière pour extraire le numéro du fichier
+    match = re.search(r'(\d+)', filename)
+    if match:
+        return int(match.group(1))
+    return 0
 
-start = 1
-painting = 1
-for i in range(1, 234):
-    start = create_opalka_painting(i, start)
-print(start)
+
+def create_montage(output_dir, montage_path, cols=12, rows=8, spacing=5, border=2, montage_width=4600):
+    # Charger les images dans l'ordre alphabétique
+    image_files = sorted([f for f in os.listdir(output_dir) if f.endswith('.png')], key=extract_number)
+    if len(image_files) != 100:
+        raise ValueError("Le dossier doit contenir exactement 100 images.")
+
+    # Largeur d'une image    
+    img_width = int((montage_width - (cols + 1) * spacing - 2 * cols * border) // cols)
+
+    # Charger une image pour obtenir sa hauteur
+    sample_img_path = os.path.join(output_dir, image_files[0])
+    sample_img = Image.open(sample_img_path)
+    sample_height = sample_img.height
+    sample_width = sample_img.width
+
+    img_height = int(img_width*sample_height/sample_width)
+    print(f"img_height: {img_height} img_width: {img_width}")
+
+    # Calculer la hauteur du montage en fonction de la hauteur des images sources
+    montage_height = rows * img_height + (rows + 1) * spacing + 2 * rows * border
+
+    print(f"montage_width: {montage_width}, montage_height: {montage_height}")
+
+    # Créer une nouvelle image pour le montage
+    montage = Image.new('RGB', (montage_width, montage_height), 'white')
+    draw = ImageDraw.Draw(montage)
+
+    # Calculer la taille de chaque image dans le montage
+    img_width = (montage_width - (cols - 1) * spacing - 2 * cols * border) // cols
+    img_height = (montage_height - (rows - 1) * spacing - 2 * rows * border) // rows
+
+    # Parcourir les images et les ajouter au montage
+    for i in range(rows):
+        for j in range(cols):
+            print(f"Image {i * cols + j + 1}")
+            img_index = i * cols + j
+            if img_index >= len(image_files):
+                break
+
+            img_path = os.path.join(output_dir, image_files[img_index])
+            img = Image.open(img_path)
+            img = img.resize((img_width, img_height))
+
+            # Calculer la position de l'image dans le montage
+            x = j * (img_width + 2 * border + spacing) + border
+            y = i * (img_height + 2 * border + spacing) + border
+
+            # Dessiner le cadre
+            draw.rectangle([x, y, x + img_width + 2 * border, y + img_height + 2 * border], outline='black', width=border)
+
+            # Coller l'image dans le montage
+            montage.paste(img, (x + border, y + border))
+
+    # Sauvegarder le montage
+    montage.save(montage_path)
+
+if False:
+    # Créer un tableau de test    
+    create_opalka_painting(30, 1)
+
+else:
+    # All painting
+    start = 1
+    painting = 1
+    for i in range(1, 101):
+        start = create_opalka_painting(i, start)
+    print(start)
+
+    montage = os.path.join(script_dir, "output", "montage.jpg")
+    create_montage(output_dir, montage)
+
